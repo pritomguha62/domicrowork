9d6eb382b4b0172e26250c89fd2acb4d97725d04
pritom gk <pritomguha62@gmail.com>
2024-10-09 23:48:35 +0600

    task update...

--- app/Models/Task_assignments.php
+++ app/Models/Task_assignments.php
@@ -16,7 +16,9 @@ class Task_assignments extends Model
     protected $fillable = [
         'task_id',
         'worker_id',
-        'worker_file',
+        'first_ss',
+        'second_ss',
+        'comment',
         'code',
         'status',
     ];
